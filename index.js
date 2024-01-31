572ca65d4335416c2454e040d0df2b783880587f
prabinnnn <prabinbhandari120@gmail.com>
2024-01-31 19:38:14 +0545

    add index

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const indexofrouter = require("./routes");
+app.use(express.json());
+app.use("/", indexofrouter);
+app.use((err, res, next, req) => {
+  err = err ? err.toString() : "something missing";
+  res.status(500).json({ msg: err });
+});
+app.listen(8000, () => {
+  console.log("app is runing");
+});
