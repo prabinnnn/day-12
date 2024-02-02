c27d3b5907379adf265ababaafe1ff7129571a8f
prabinnnn <prabinbhandari120@gmail.com>
2024-02-02 17:31:49 +0545

    add index

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = Number(procee.env.PORT);
 const indexofrouter = require("./routes");
-app.use(express.json());
 app.use("/", indexofrouter);
 mongoose
   .connect(process.env.DB)
   .then(() => console.log("Connected!"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
+app.use(express.json());
 app.use((err, res, next, req) => {
   err = err ? err.toString() : "something missing";
   res.status(500).json({ msg: err });
--- module/book/book.model.js
+++ module/book/book.model.js
@@ -1,5 +1,9 @@
 const { schema, model } = require("mongoose");
 const BookSchema = schema({
   title: { type: String, required: true },
+  author: { type: Array, required: true },
+  pageno: { type: Number, required: true },
+  updatedat: { type: Date, default: Date.now() },
+  createdat: { type: Date, default: Date.now() },
 });
 module.exports = model("book", BookSchema);
