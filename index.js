b3f87121134791069b944bf19466e230548576e4
prabinnnn <prabinbhandari120@gmail.com>
2024-02-02 17:04:47 +0545

    add index

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const PORT = Number(procee.env.PORT);
 const indexofrouter = require("./routes");
 app.use(express.json());
 app.use("/", indexofrouter);
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 app.use((err, res, next, req) => {
   err = err ? err.toString() : "something missing";
   res.status(500).json({ msg: err });
 });
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log("app is runing");
 });
